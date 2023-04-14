c6af0755368746b8d9d5380779e8b3565ce02c72
CryptoSpoon <53559585+dovedko@users.noreply.github.com>
2023-04-14 22:42:53 +0300

    Input castom gwei and transaction waiting time

--- send.py
+++ send.py
@@ -10,7 +10,6 @@ from mnemonic import Mnemonic
 from bip32utils import BIP32Key, BIP32_HARDEN
 import re
 
-
 # Function to send transaction
 def send_transaction(credential):
     # Check if the input is a mnemonic phrase or private key
@@ -39,14 +38,15 @@ def send_transaction(credential):
     nonce = w3.eth.getTransactionCount(address)
 
     # Set the transaction parameters
-    gas_price = w3.eth.gasPrice
+    chain_id = 56
     tx_params = {
         "nonce": nonce,
         "gasPrice": gas_price,
         "gas": gas_limit,
         "to": contract_address,
         "value": 0,
-        "data": data
+        "data": data,
+        "chainId": chain_id,
     }
 
     # Check if the account balance is sufficient to cover the gas fees
@@ -63,7 +63,7 @@ def send_transaction(credential):
     tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
 
     # Wait for the transaction to be mined
-    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash, timeout=transaction_wait_time)
 
     # Check the transaction status
     if tx_receipt["status"] == 1:
@@ -75,15 +75,20 @@ def send_transaction(credential):
         print(f"Failed: {tx_hash.hex()}")
         return False
 
-# Set the Binance Smart Chain endpoint URL
-w3 = web3.Web3(web3.Web3.HTTPProvider("https://bsc-dataseed.binance.org/"))
+# Set the Binance Smart Chain endpoint URL (I recommend to use custom ankr RPC)
+w3 = web3.Web3(web3.Web3.HTTPProvider("https://rpc.ankr.com/bsc"))
 
 # Get the contract address, data, and gas limit from user input
-contract_address_input = input("Enter the contract address: ")
+contract_address_input = input("Enter the contract address (Interacted With (To) - address from bscscan): ")
 contract_address = HexBytes(contract_address_input)
-data_input = input("Enter the function data: ")
+data_input = input("Enter the function data (Input Data - from bscscan): ")
 data = HexBytes(data_input)
-gas_limit = int(input("Enter the gas limit: "))
+gas_limit = int(input("Enter the gas limit (50000 - send transaction, 500000 - contract interaction): "))
+
+gas_price_gwei = float(input("Enter the gas price in gwei (e.g., 1.2): "))
+gas_price = int(gas_price_gwei * 1e9)
+
+transaction_wait_time = int(input("Enter the transaction wait time in seconds (e.g., 300): "))
 
 # Load the credentials from a file
 with open("private_keys.txt", "r") as f:
@@ -94,7 +99,7 @@ completed_count = 0
 failed_count = 0
 
 # Get the number of threads from user input
-num_threads = int(input("Enter the number of threads: "))
+num_threads = int(input("Enter the number of threads (number of your wallets): "))
 
 # Iterate over the credentials and send a transaction for each one using multithreading
 with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
